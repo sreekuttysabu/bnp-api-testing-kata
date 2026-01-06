40463f78097cb64dd7f3ebe661a46b7d941e20f7
sreekutty sabu <sreekuttysabu11@gmail.com>
2026-01-06 12:06:26 +0100

    Increase room id range to reduce booking conflicts

--- src/test/java/com/booking/utils/BookingPayloadUtil.java
+++ src/test/java/com/booking/utils/BookingPayloadUtil.java
@@ -2,7 +2,10 @@ package com.booking.utils;
 
 import java.time.LocalDate;
 import java.util.Random;
-
+/**
+ * Utility class for creating booking request payloads with dynamic data
+ * to keep tests stable in a shared environment.
+ */
 public class BookingPayloadUtil {
 
     private static final Random RANDOM = new Random();
@@ -13,7 +16,7 @@ public class BookingPayloadUtil {
     ) {
         String checkIn = LocalDate.now().plusDays(10).toString();
         String checkOut = LocalDate.now().plusDays(12).toString();
-        int roomId = RANDOM.nextInt(100) + 1;
+        int roomId = RANDOM.nextInt(100000) + 1;
 
         return """
             {
